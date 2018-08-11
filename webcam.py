237f4ea47eeedfc1630f49c216a37ddd7f0ef233
Francois Guibert <frozax@frozax.com>
2018-08-11 17:59:35 +0200

    logging

---
+++ webcam.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import logging
+
+logging.basicConfig(filename="webcam.log", level=logging.DEBUG, format="%(asctime)s [%(levelname)] %(message)s")
+
+logging.info("TOTO")
+logging.warning("TOTO")

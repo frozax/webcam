643bb021b701dea149c472610a59087eabbe4538
Francois Guibert <frozax@frozax.com>
2018-08-12 13:18:53 +0200

    convert

--- convert.py
+++ convert.py
@@ -8,13 +8,32 @@ from datetime import datetime
 from webcam import PATH, EXT
 
 
+D1 = "120fps"
+
+
+def convert(f):
+    logging.info("Converting %s" % f)
+    cmd_line = "exec ffmpeg -i " + str(f)
+    cmd_line += " -hide_banner"
+    cmd_line += " -r 120"
+    cmd_line += " -filter:v \"setpts=0.1*PTS\""
+    cmd_line += " " + str(PATH / D1 / f.name)
+
+    logging.info("New convert: %s", cmd_line)
+    Popen(cmd_line, shell=True).wait()
+
+
 def loop():
     while True:
-        files = sorted(list(PATH.glob("*." + EXT)))
-        print(files)
+        # get all files except last two
+        converted = sorted(list((PATH / D1).glob("*." + EXT)))
+        converted = [c.name for c in converted]
+        to_convert = sorted(list(PATH.glob("*." + EXT)))[:-2]
+        for file in to_convert:
+            if file.name not in converted:
+                convert(file)
         break
         time.sleep(10)
-
 
 
 if __name__ == '__main__':
